420ea3590dcedc90fe02341e09c663dac05cfded
fazil.s@lantrasoft.com <SinCos123>
2020-09-30 15:11:38 +0530

    Ids tested

--- app/src/androidTest/java/com/example/uitesting/SecondaryActivityTest.kt
+++ app/src/androidTest/java/com/example/uitesting/SecondaryActivityTest.kt
@@ -1,10 +1,16 @@
 package com.example.uitesting
 
 import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.ViewAssertion
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import org.junit.Assert.*
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4ClassRunner::class)
@@ -12,4 +18,25 @@ class SecondaryActivityTest{
     @get:Rule
     val activityScenariorule=ActivityScenarioRule(SecondaryActivity::class.java)
 
+    @Test
+    fun test_isActivityIdPresent_isPresent() {
+
+        onView(ViewMatchers.withId(R.id.secondary))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        onView(ViewMatchers.withId(R.id.button_previous))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        onView(ViewMatchers.withId(R.id.activity_secondary_title))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        onView(ViewMatchers.withId(R.id.secondary))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun test_isButtonName_isDispalyed() {
+        onView(ViewMatchers.withId(R.id.activity_secondary_title))
+            .check(ViewAssertions.matches(ViewMatchers.withText(R.string.text_secondaryactivity)))
+    }
 }
